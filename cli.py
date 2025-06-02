36edc7a0aa728e858bd76dec52108de4d92b7dec
EtherBird <145231985+R0salman@users.noreply.github.com>
2025-06-02 20:57:51 +0300

    Update cli.py version

--- cli.py
+++ cli.py
@@ -9,7 +9,7 @@ import asyncio
 from rich.console import Console
 import datetime
 
-VERSION = "0.1.1"
+VERSION = "0.1.5"
 
 from openrecon.scanners.portScanner import run_port_scan
 from openrecon.scanners.netDisc import live_discovery
